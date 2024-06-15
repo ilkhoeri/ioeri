6080d6bced5afa449d37c78df3c3cf4b4fa4a4b1
ilkhoeri <khoeriilham@gmail.com>
2024-06-15 20:49:17 +0700

    Update page.tsx

--- app/(routes)/playground/[playgroundId]/page.tsx
+++ app/(routes)/playground/[playgroundId]/page.tsx
@@ -1,15 +1,14 @@
 import { Metadata } from "next";
 import { Article } from "@/components/ui/component";
 import { TitlePageID } from "@/components/clients/title-page";
-// import { MarkdownEditor } from "../playground-markdown-editor";
-import MarkdownEditorRest from "./rest";
+import { MarkdownEditor } from "../playground-markdown-editor";
 // import { notFound } from "next/navigation";
 
 import fs from "fs-extra";
 
 type Params = { params: { playgroundId: string } };
 
-export const fetchCache = "only-no-store";
+// export const fetchCache = "only-no-store";
 
 export async function generateMetadata({ params }: Params): Promise<Metadata> {
   const url = process.env.NEXT_PUBLIC_DOMAIN_URL;
@@ -29,13 +28,17 @@ export async function generateMetadata({ params }: Params): Promise<Metadata> {
 }
 
 export default async function Page({ params }: Params) {
-  const edit = await fs.readFile(process.cwd() + "/md/markdown.md", "utf-8");
+  const [edit, code, css] = await Promise.all([
+    fs.readFile(process.cwd() + "/md/markdown.md", "utf-8"),
+    fs.readFile(process.cwd() + "/modules/utils/formatter/mardown-text.ts", "utf-8"),
+    fs.readFile(process.cwd() + "/modules/utils/formatter/markdown.css", "utf-8"),
+  ]);
   // const data = JSON.parse(file);
   return (
     <Article>
       <TitlePageID title="Markdown Editor" />
 
-      <MarkdownEditorRest edit={edit} />
+      <MarkdownEditor edit={edit} code={code} css={css} />
     </Article>
   );
 }
