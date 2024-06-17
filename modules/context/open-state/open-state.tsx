14559b7beec518a7e0d2b8f9a8df1eacb6e3e857
ilkhoeri <khoeriilham@gmail.com>
2024-06-17 13:42:15 +0700

    🪝

--- modules/context/open-state/open-state.tsx
+++ modules/context/open-state/open-state.tsx
@@ -11,7 +11,7 @@ interface OpenStateContextProps {
   setOpen: (value: boolean) => void;
   handleOpen: () => void;
   handleClose: () => void;
-  onClick: () => void;
+  onHandle: () => void;
   onMouseEnter: () => void;
   onMouseLeave: () => void;
   onKeyDown: () => void;
