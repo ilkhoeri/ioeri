4caa54e1e8fde87c8e121add6544af7d5859b50a
ilkhoeri <khoeriilham@gmail.com>
2024-06-20 23:48:18 +0700

    ...

--- modules/components/index.ts
+++ modules/components/index.ts
@@ -8,8 +8,3 @@ export {
   CollapsibleTrigger,
   useCollapsibleContext,
 } from "./web/collapsible/collapsible";
-
-export { Svg as SVGParent } from "./web/svg/svg";
-export { InitialSize, SvgBase, SvgIcon, getSizes, getInitialSizes, getSvg } from "./web/svg/svg";
-
-export { default as Element } from "./web/element/element";
--- modules/index.ts
+++ modules/index.ts
@@ -5,10 +5,7 @@ export * from "./context";
 export * from "./types";
 export * from "./functions/cnx/cnx";
 export * from "./development-components";
+export * from "./components/web/svg/svg";
+export * from "./components/web/element/element";
 // ioeri-icons
 export * from "./icons";
-
-// 
-export * from "./components/web/svg/svg";
-
-export * from "./components/web/element/element";
