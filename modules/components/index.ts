7eea36482bc6c211a4de76ec78982bbe33620e0c
ilkhoeri <khoeriilham@gmail.com>
2024-06-20 23:44:33 +0700

    ...

--- modules/components/index.ts
+++ modules/components/index.ts
@@ -11,7 +11,5 @@ export {
 
 export { Svg as SVGParent } from "./web/svg/svg";
 export { InitialSize, SvgBase, SvgIcon, getSizes, getInitialSizes, getSvg } from "./web/svg/svg";
-export type { Colors, DetailedSvg, IconTree, IconType, Sizes, SizesProps } from "./web/svg/svg";
 
 export { default as Element } from "./web/element/element";
-export type { PolymorphicRef, PolymorphicWithoutRef } from "./web/element/element";
--- modules/index.ts
+++ modules/index.ts
@@ -9,10 +9,6 @@ export * from "./development-components";
 export * from "./icons";
 
 // 
-export { default as Svg } from "./components/web/svg/svg";
-export { Svg as SVGParent } from "./components/web/svg/svg";
-export { InitialSize, SvgBase, SvgIcon, getSizes, getInitialSizes, getSvg } from "./components/web/svg/svg";
-export type { Colors, DetailedSvg, IconTree, IconType, Sizes, SizesProps } from "./components/web/svg/svg";
+export * from "./components/web/svg/svg";
 
-export { default as Element } from "./components/web/element/element";
-export type { PolymorphicRef, PolymorphicWithoutRef } from "./components/web/element/element";
+export * from "./components/web/element/element";
