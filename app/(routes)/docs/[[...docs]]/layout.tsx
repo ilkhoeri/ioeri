2ac7ea8f229ad28e8964a54ab1980047ffd0d6ff
ilkhoeri <khoeriilham@gmail.com>
2024-07-03 10:49:20 +0700

    🔥

--- app/(routes)/docs/[[...docs]]/layout.tsx
+++ app/(routes)/docs/[[...docs]]/layout.tsx
@@ -3,9 +3,17 @@ import RestDocsPage from "./client";
 
 import { notFound } from "next/navigation";
 
+import { Section, Main } from "@/library/components/components";
 import { getPath, getPaths } from "@/library/scripts/get-paths";
+import { AsideLeft } from "@/library/assets/nav-aside-left";
+import { AsideRight } from "@/library/assets/nav-aside-right";
+import { NavBottom } from "@/library/assets/nav-prev-next";
+import { NavigationBreadcrumb } from "@/library/assets/nav-breadcrumb";
 import { InnerRoutes, NestedRoute, SingleRoute } from "@/library/routes";
 
+export const runtime = "nodejs";
+export const dynamicParams = true;
+export const dynamic = "force-dynamic";
 // export const fetchCache = "only-no-store";
 // export function generateStaticParams() {
 //   return [{ docs: ["hooks", "use-clipboard"] }, { docs: ["b", "2"] }, { docs: ["c", "3"] }];
@@ -31,8 +39,26 @@ export default async function Layout({ children, params }: Readonly<DocsParams>)
   const utility = await loadRoutes("utility");
   const hooks = await loadRoutes("hooks");
 
+  function Template({ children }: { children: React.ReactNode }) {
+    return (
+      <Main>
+        <AsideLeft topRoutes={[...utility]} routes={[...hooks]} nestedRoutes={nested} />
+        <Section>
+          <NavigationBreadcrumb />
+          {children}
+          <NavBottom routes={[...utility, ...components, ...hooks]} />
+        </Section>
+        <AsideRight />
+      </Main>
+    );
+  }
+
   if (!params.docs) {
-    return <Docs />;
+    return (
+      <Template>
+        <Docs />
+      </Template>
+    );
   }
 
   if (params.docs.length === 1) {
@@ -42,13 +68,17 @@ export default async function Layout({ children, params }: Readonly<DocsParams>)
       hooks: hooks,
     };
     const routes = routesMap[params.docs[0]];
-    return <RestDocsPage id={params.docs[0]} routes={routes} />;
+    return (
+      <Template>
+        <RestDocsPage id={params.docs[0]} routes={routes} />
+      </Template>
+    );
   }
 
   const matchingRoutes = findMatchingRoute(params.docs, [...components, ...utility, ...hooks]);
   if (!matchingRoutes) notFound();
 
-  return <>{children}</>;
+  return <Template>{children}</Template>;
 }
 
 const findMatchingRoute = (slug: string[], routes: (InnerRoutes | SingleRoute)[]): boolean => {
