c9eb4d225d6551de21f95ce1535f1f9baf178a07
ilkhoeri <khoeriilham@gmail.com>
2024-07-10 13:56:55 +0700

    🔥

--- app/(routes)/examples/layout.tsx
+++ app/(routes)/examples/layout.tsx
@@ -1,9 +1,34 @@
 import { Section, Main } from "@/library/components/components";
+import { getPath, getPaths } from "@/library/scripts/get-paths";
 
-export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+import { Headnav } from "@/library/assets/nav-head";
+import { NestedRoute, SingleRoute } from "@/library/routes";
+
+async function routes(sourcePath: string): Promise<SingleRoute[]> {
+  return await getPath(["resource", "docs", sourcePath]);
+}
+async function nestedRoutes(sourcePath: string): Promise<NestedRoute[]> {
+  return await getPaths(["resource", "docs", sourcePath]);
+}
+async function examplesRoutes(sourcePath: string): Promise<SingleRoute[]> {
+  return await getPath(["resource", sourcePath]);
+}
+
+export default async function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const [utility, nested, hooks, examples] = await Promise.all([
+    routes("utility"),
+    nestedRoutes("components"),
+    routes("hooks"),
+    examplesRoutes("examples"),
+  ]);
+
   return (
-    <Main>
-      <Section className="px-6 md:px-8 lg:px-12">{children}</Section>
-    </Main>
+    <>
+      <Headnav routes={[...utility, ...nested, ...hooks, ...examples]} />
+
+      <Main>
+        <Section className="px-6 md:px-8 lg:px-12">{children}</Section>
+      </Main>
+    </>
   );
 }
